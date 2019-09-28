94c96ce375fc9d484af7fb6f6b1e0e44d173d72c
HCS Learning <learning@hardcybersoft.cl>
2019-09-28 02:32:15 -0300

    se agrega dependencia a Bundle Catalogo

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use HCS\SluggerBundle\Service\HCSSluggerService;
 use Symfony\Component\HttpFoundation\Request;
+use HCS\CatalogoBundle\Entity\Producto;
 
 class IndexController extends AbstractController
 {
@@ -14,12 +15,14 @@ class IndexController extends AbstractController
      */
     public function index(Request $request, HCSSluggerService $slugger)
     {
+        $p = (new Producto())->setNombre('Smart TV LG 32"');
         $titulo = $request->query->get("titulo", "");
         $slug = $slugger->slugify($titulo);
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'titulo' => $titulo,
-            'slug'  => $slug
+            'slug'  => $slug,
+            'producto' => $p,
         ]);
     }
 }
